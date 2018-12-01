fcca917fb445665dc5c62c5fb2715c7a87d311f7
Zach Ruffin <zachruffin@gmail.com>
2018-12-01 15:14:54 -0500

    Finished puzzle 1.

--- AdventCode_1/Program.cs
+++ AdventCode_1/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
 
 namespace AdventCode_1
 {
@@ -6,7 +9,42 @@ namespace AdventCode_1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Dictionary<int, int> visitedFrequencies = new Dictionary<int, int>();
+            int currentFrequency = 0;
+            bool foundDuplicateFrequency = false;
+
+            string[] frequencyDeltas = File.ReadAllLines(@"input.txt");
+
+            while (foundDuplicateFrequency == false)
+            {
+                foreach (string inputDelta in frequencyDeltas)
+                {
+
+                    bool isValidInput = int.TryParse(inputDelta.Trim(), out int currentFrequencyDelta);
+
+                    if (!isValidInput)
+                    {
+                        Debug.WriteLine(string.Format("Invalid input detected: '{0}' is not a number.", inputDelta.Trim()));
+                    }
+                    else
+                    {
+                        currentFrequency += currentFrequencyDelta;
+
+                        if (visitedFrequencies.ContainsKey(currentFrequency))
+                        {
+                            visitedFrequencies[currentFrequency]++;
+                            Console.WriteLine("Duplicate Frequency Detected at: {0}!", currentFrequency);
+                            foundDuplicateFrequency = true;
+                            break;
+                        }
+
+                        visitedFrequencies.Add(currentFrequency, 1);
+                    }
+                }
+            }
+            
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();
         }
     }
 }
