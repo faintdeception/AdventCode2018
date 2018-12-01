1fb31b7e1f602914d8c33d336c7e35c29878b38f
Zach Ruffin <zachruffin@gmail.com>
2018-12-01 14:04:57 -0500

    Add project files.

---
+++ AdventCode_1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AdventCode_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
